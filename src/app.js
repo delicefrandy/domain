2a3b9d9f27e4479fe3855e2fda0804134c98eb5a
Frandy Delice <delicefrandy103@gmail.com>
2024-04-27 16:14:40 -0400

    Updated app.js, index.html, style.css

--- src/app.js
+++ src/app.js
@@ -1,24 +1,28 @@
-const changeText = document.getElementById('btn');
-const showRandom = document.getElementById('show-random');
+const GenDomain = document.getElementById('domain');
+const showRandom = document.getElementById('show-domain');
 
 
-let who = ['The dog', 'My grandma', 'His turtle', 'My bird', 'Our Fox', 'That Tiger', 'This Wolf', 'The Zebra', 'My Deer', 'Our Rabbit', 'One Jaguar', 'All Squirrel'];
+let pronoun = ['the', 'my', 'his', 'your', 'mine', 'we'];
+let adj = ['odd', 'crazy', 'small', 'big', 'blue', 'pretty'];
+let noun = [ 'keys', 'car', 'food', 'book', 'house', 'toy', 'window', 'mouse', 'dog'];
 
-let action = ['ate', 'peed', 'crushed', 'broke', 'aide', 'analyzed', 'assigned', 'checked', 'chose', 'concluded', 'dealt', 'defined', 'acquired', 'appeard', 'cleared', 'decided'];
 
-let what = ['my homework', 'the keys', 'the car', 'my food', 'my only book', 'the house', 'the toy', 'the window', 'one time again', 'one another day'];
-
-let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying', 'when  was sleeping', 'being on a trip', 'while I was bathimg'];
-const random = (()=>{
-  const randomWho = Math.round(Math.random()    *(who.length-1));
-  const randomAction = Math.round(Math.random() *(action.length-1));
-  const randomWhat = Math.round(Math.random()   *(what.length-1));
-  const randomWhen = Math.round(Math.random()   *(when.length-1));
-  console.log(randomAction);
-  
-  showRandom.textContent=`${who[randomWho]} ${action[randomAction]} ${what[randomWhat]} ${when[randomWhen]}`;
+const generatorDomain = (()=>{
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        
+        const theDomain = document.createElement("p");
+        theDomain.classList="my-domain"
+        showRandom.appendChild(theDomain)
+        console.log(pronoun[i]+adj[j])
+        theDomain.innerHTML =  `<a href="#">${pronoun[i]}${adj[j]}${noun[k]}.com</a> `
+        
+      }
+    
+   }
+  }
 })
+GenDomain.addEventListener('click', generatorDomain);
 
-changeText.addEventListener('click', random);
 
-random();
